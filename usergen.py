07d9006a256eb1c1f0baa4c89b749d04bf1962e7
krubottom <karl.rubottom@gmail.com>
2016-11-24 11:12:50 -0700

    First Commit

---
+++ usergen.py
@@ -0,0 +1,17 @@
+import json
+import urllib2
+
+gen_url = 'https://randomuser.me/api/?nat=us&results=50'
+
+f = urllib2.urlopen(gen_url)
+json_string = f.read()
+parsed_json = json.loads(json_string)
+
+for user in parsed_json['results']:
+    print("First Name: " + user['name']['first'].title())
+    print("Last Name: " + user['name']['last'].title())
+    print("Username: " + user['login']['username'])
+    print("Password: " + user['login']['password']) + "\n"
+
+# print("First Name: " +  parsed_json['results'][0]['name']['first'])
+# print("Last Name: " + last_name = parsed_json['results'][0]['name']['last'])
